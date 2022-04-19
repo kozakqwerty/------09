871af159d6752057c46d365fd2e430724d7b5a24
kozakqwerty <diman_kozak@mail.ru>
2022-04-19 13:27:25 +0300

    nav

--- js/index.js
+++
@@ -1,9 +0,0 @@
-import modalAppearance from "./modules/modalop.js";
-import modalClose from "./modules/modalcl.js";
-import accordionsOpening from "./modules/opening.js";
-import constructorAccordion from "./modules/constructor.js";
-
-modalAppearance();
-modalClose();
-accordionsOpening();
-constructorAccordion();
---
+++ js/main.js
@@ -0,0 +1,9 @@
+import modalAppearance from "./modul/modalop.js";
+import modalClose from "./modul/modalcl.js";
+import accordionsOpening from "./modul/opening.js";
+import constructorAccordion from "./modul/constructor.js";
+
+modalAppearance();
+modalClose();
+accordionsOpening();
+constructorAccordion();
